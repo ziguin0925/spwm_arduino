774aba7296e84715b483795c20c1cdc454bc32a5
RG <117524772+ziguin0925@users.noreply.github.com>
2024-05-09 14:00:24 +0900

    Update spwm_basic.ino
    두 개의 상반된 PWM신호로 설정
    Phase and Frequency Correct PWM Mode 로 변경
    삼각파의 주파수 10kHz 로 변경

--- spwm_basic.ino
+++ spwm_basic.ino
@@ -1,68 +1,90 @@
-/*
- * sPWMv2.c
- *
- * Created: 31/12/2014 3:44:43 PM
- *  Author: Kurt Hutten
- sPWM on the atMega328 for the arduino Uno, might be compatable with other atmel chips / arduino boards.
- Compare outputs A and B output to PORTB pins 1 and 2 which are pins 9 and 10 respectively on the Uno.
- Also useful to know the led on the Uno is pin 5 on PORTB.
- */ 
-//https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
-//http://artoa.hanbat.ac.kr/lecture_data/microprocessor/2014/%EC%A0%9C7%EC%9E%A5%20%ED%83%80%EC%9D%B4%EB%A8%B8%EC%99%80%20PWM%20(HBE-MCU-Multi%20AVR).pdf
-
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-// Look up tables with 20 entries each, normalised to have max value of 80 which is the period of the PWM loaded into register ICR1.
-int lookUp1[] = {8, 24, 40, 56, 80, 56, 40, 24, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};//[9]=0
-int lookUp2[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 24, 40, 56, 80, 56, 40, 24, 8, 0};//[9]=0
+#define _MATH_DEFINES_DEFINED
+
+#define N 20
+
+//http://artoa.hanbat.ac.kr/lecture_data/microprocessor/2014/%EC%A0%9C7%EC%9E%A5%20%ED%83%80%EC%9D%B4%EB%A8%B8%EC%99%80%20PWM%20(HBE-MCU-Multi%20AVR).pdf
+
+/*
+
+지령파의 한 주기안에 삼각파가 20개 들어가도록 설정, 지령파가 변할수록 동적으로 lookUp table의 배열도 바뀌어야 함.
+ICR1 최대 0-799로 설정
+
+*/
+static unsigned int lookUp1[20];
+static unsigned int lookUp2[20];
+
+double temp;
+int num_ch;
 /*
 lookUp1[]={듀티 결정}
-sizeof(lookUp1):주파수 결정
+sizeof(lookUp1): 지령파 내의 삼각파 갯수 결정 
 */
 
+
 void setup(){
+  
+  for(int i = 0; i < 20; i++){ 
+    temp = 400*(sin(i*2*M_PI/20)+1);
+    lookUp2[i] = (int)(temp+0.5); //강제 형변환 (round)
+  }
+
+  memcpy(lookUp1, lookUp2, sizeof(lookUp2));
+
+/*
+  for (int i = 0; i < N / 2; i++)
+	{
+		num_ch = lookUp1[i];
+		lookUp1[i] = lookUp1[N - i - 1]; // 요소가 20개일때 마지막 요소는 9
+		lookUp1[N - i - 1] = num_ch;
+	}
+
+  lookUp2 가 완전한 spwm의 파형이 안나오므로 com레지스터 인터럽트를 OC1A가 켜질 때와 반대로 되도록 설정함.
+*/
     // Register initilisation, see datasheet for more detail.
-    TCCR1A = 0b10100010; //(Timer/Counter Control Register)
+    TCCR1A = 0b10110010; //(Timer/Counter Control Register)
 
        /*10 TCNT가 OCR1A와 일치해지면 1출력(COM1A1, COM1A0)
-         10 (COM1B1, COM1B0)
+         11 (COM1B1, COM1B0) reverse
          00
-         10 WGM1 1:0 for waveform 15.
+         10 WGM1 1:0 
        */
-    TCCR1B = 0b00011001;
+    TCCR1B = 0b00000001;
        /*000
-         11 WGM1 3:2 for waveform 15.
+         00 WGM1 3:2 
          001 prescale= 1.
 
          TCCR1A의 WGM1 1:0과  TCCR1B WGM1 3:2 합치면 1110(option = Fast PWM, TOP=ICR1 )
+         1000 : PWM,Phase and Frequency
+         TOP : OCRnA
+         Update of OCRnx at : BOTTOM
+         TOVn Flag Set on : BOTTOM
+Correct
        */
     TIMSK1 = 0b00000001;//(Interrupt Mask Register)
        /*0000000
          1 TOV1 Flag interrupt enable(타이머 오버플로우 인터럽트)
        */
 
-    ICR1   = 79;     // 비교기(TCNT최대 증가치) Period for 16MHz crystal (클럭 16MHz) 
-                      //a switching frequency of 200KHz
-                      //20 subdevisions per 10KHz sin wave cycle.
+    ICR1   = 799;     // 비교기(TCNT최대 증가치) Period for 16MHz crystal (클럭 16MHz) 
+                      // (삼각파의 주파수를 10kHz로 줘야함)
     sei();             // Enable global interrupts.
-    DDRB = 0b00000110; // Set PB1 and PB2 as outputs. 핀설정9,10
+
+    DDRB = 0b01100000; // Set outputs. pwm 11,12 pin
     pinMode(13,OUTPUT);
 }
 
-void loop(){; /*Do nothing . . . . forever!*/}
+void loop(){; }
 
 ISR(TIMER1_OVF_vect){
-    static int num;
+    static int num =0 ;
     static char trig;
     // change duty-cycle every period.
     OCR1A = lookUp1[num];//OC1A
     OCR1B = lookUp2[num];//OC1B
-    
-    if(++num >= 20){ // Pre-increment num then check it's below 40. 없어도 될듯
-       num = 0;       // Reset num.
-       trig = trig^0b00000001;
-       digitalWrite(13,trig); //13번 핀을 통한 HIGH, LOW 제어부분
+     if(++num >= 20){
+      num=0;
      }
 }
